e5d1f3d6cbda9185ccbe6c4c1fc358febb2db9b6
sycy600 <sycy600@gmail.com>
2013-09-07 12:16:01 +0200

    init

---
+++ rslib/polynomial.h
@@ -0,0 +1,10 @@
+// Copyright 2013 sycy600
+
+#ifndef RSLIB_POLYNOMIAL_H_
+#define RSLIB_POLYNOMIAL_H_
+
+int fun() {
+    return 2;
+}
+
+#endif  // RSLIB_POLYNOMIAL_H_
---
+++ test/polynomial_tests.cc
@@ -0,0 +1,8 @@
+// Copyright 2013 sycy600
+
+#include "gtest/gtest.h"
+#include "rslib/polynomial.h"
+
+TEST(HelloWorld, Test) {
+    ASSERT_EQ(fun(), 2);
+}
