fb0e7a4502b8a419d29acd5cbcfaab3d62e7f877
sycy600 <sycy600@gmail.com>
2013-09-22 18:05:38 +0200

    basic arithmetic fo simple field

--- rslib/simplefieldelement.cc
+++ rslib/simplefieldelement.cc
@@ -10,4 +10,102 @@ SimpleFieldElement getZero(SimpleFieldElement elem) {
   return SimpleFieldElement(0, sf);
 }
 
+SimpleFieldElement::SimpleFieldElement(unsigned int value,
+                                       const SimpleField& simpleField)
+  : simpleField_(simpleField) {
+  value_ = value % simpleField.getCharacteristic();
+}
+
+SimpleFieldElement& SimpleFieldElement::operator=(
+    const SimpleFieldElement& other) {
+  // Assigned element must be in the same field.
+  checkIfTheSameField(other);
+  value_ = other.getValue();
+  return *this;
+}
+
+SimpleFieldElement& SimpleFieldElement::operator+=(
+    const SimpleFieldElement& other) {
+  checkIfTheSameField(other);
+  *this = getField().add(*this, other);
+  return *this;
+}
+
+SimpleFieldElement SimpleFieldElement::operator-() const {
+  return getField().additiveInverse(*this);
+}
+
+SimpleFieldElement& SimpleFieldElement::operator-=(
+    const SimpleFieldElement& other) {
+  *this += -other;
+  return *this;
+}
+
+SimpleFieldElement& SimpleFieldElement::operator*=(
+    const SimpleFieldElement& other) {
+  checkIfTheSameField(other);
+  *this = getField().multiply(*this, other);
+  return *this;
+}
+
+SimpleFieldElement SimpleFieldElement::multiplicativeInverse() const {
+  return getField().multiplicativeInverse(*this);
+}
+
+SimpleFieldElement& SimpleFieldElement::operator/=(
+    const SimpleFieldElement& other) {
+  *this *= other.multiplicativeInverse();
+  return *this;
+}
+
+void SimpleFieldElement::checkIfTheSameField(
+    const SimpleFieldElement& other) const {
+  if (getField() != other.getField()) {
+    throw SimpleFieldElementException(
+          "Assignment to field element from different field");
+  }
+}
+
+bool operator==(const SimpleFieldElement& first,
+                const SimpleFieldElement& second) {
+  if (first.getField() != second.getField()) {
+    throw SimpleFieldElementException(
+          "Assignment to field element from different field");
+  }
+  return first.getValue() == second.getValue();
+}
+
+bool operator!=(const SimpleFieldElement& first,
+                const SimpleFieldElement& second) {
+  return !(first == second);
+}
+
+SimpleFieldElement operator+(const SimpleFieldElement& first,
+                             const SimpleFieldElement& second) {
+  SimpleFieldElement result(first);
+  result += second;
+  return result;
+}
+
+SimpleFieldElement operator-(const SimpleFieldElement& first,
+                             const SimpleFieldElement& second) {
+  SimpleFieldElement result(first);
+  result -= second;
+  return result;
+}
+
+SimpleFieldElement operator*(const SimpleFieldElement& first,
+                             const SimpleFieldElement& second) {
+  SimpleFieldElement result(first);
+  result *= second;
+  return result;
+}
+
+SimpleFieldElement operator/(const SimpleFieldElement& first,
+                             const SimpleFieldElement& second) {
+  SimpleFieldElement result(first);
+  result /= second;
+  return result;
+}
+
 }  // namespace rslib
