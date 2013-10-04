6982bfe8043ddfa6a05228b1450ca8e1c7835f8e
sycy600 <sycy600@gmail.com>
2013-10-04 22:53:16 +0200

    Update run_doxygen.sh

--- tools/run_doxygen.sh
+++ tools/run_doxygen.sh
@@ -5,8 +5,12 @@ then
   exit 1
 fi
 
+# Load configuration + override INPUT parameter.
 (cat $2; echo INPUT=$1) | doxygen -
+
+# Show errors (if any).
 cat doxygen_warnings.txt
+
 # If this file is not empty then there are some documentation errors.
 if [ -s doxygen_warnings.txt ]
 then
